00187b55015ad6d44a64c734b5df3226254e77f8
Chalitha Laksahan <141056409+chalithalaksahan@users.noreply.github.com>
2026-02-01 09:17:21 +0530

    bitwise operators

--- JavaScirpt/app.js
+++ JavaScirpt/app.js
@@ -1,1 +1,30 @@
 console.log("Hello, World!");
+/*
+                    What Are Bitwise Operators, and How Do They Work ?
+    
+                    These operators perform operations on the binary representations of numbers.JavaScript provides several bitwise operators, 
+     including AND(&), OR(|), XOR(^), NOT(~), left shift(<<), and right shift(>>).
+
+        let a = 5;  // Binary: 101
+        let b = 3;  // Binary: 011
+        console.log(a & b);  // 1 (Binary: 001)
+
+        let a = 5;  // Binary: 101
+        let b = 3;  // Binary: 011
+        console.log(a | b);  // 7 (Binary: 111)
+
+        let a = 5;  // Binary: 101
+        let b = 3;  // Binary: 011
+        console.log(a ^ b);  // 6 (Binary: 110)
+
+        let a = 5;  // Binary: 101
+        console.log(~a);  // -6
+
+        let a = 5;  // Binary: 101
+        console.log(a << 1);  // 10 (Binary: 1010)
+
+        let a = 5;  // Binary: 101
+        console.log(a >> 1);  // 2 (Binary: 10)
+
+
+     */

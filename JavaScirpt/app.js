e1aa2c74de3e9075c016ab33269573c7d1f47345
Chalitha Laksahan <141056409+chalithalaksahan@users.noreply.github.com>
2026-02-01 13:33:35 +0530

    How Does isNaN Work?

--- JavaScirpt/app.js
+++ JavaScirpt/app.js
@@ -264,16 +264,59 @@ console.log("Hello, World!");
         const randomNumBtw1And20 = Math.floor(Math.random() * 20) + 1;      //GENERATED RANDOM NUMBER BETWEEN 1 AND 20
         console.log(randomNumBtw1And20);
 
+
+
         
+               //////////////////////////////////////////////////////////////////////////////////////
+                                                 How Does isNaN Work?
+               /////////////////////////////////////////////////////////////////////////////////////
+
+
+        let result = 0 / 0;
+        console.log(result); // NaN
+
+        console.log(NaN === NaN); // false
+
+
+        ---------------------------------not recomended---------------------------------------------------
+        console.log(isNaN(NaN));       // true
+        console.log(isNaN(undefined)); // true
+        console.log(isNaN({}));        // true
+
+        console.log(isNaN(true));      // false
+        console.log(isNaN(null));      // false
+        console.log(isNaN(37));        // false
+
+        console.log(isNaN("37"));      // false: "37" is converted to 37
+        console.log(isNaN("37.37"));   // false: "37.37" is converted to 37.37
+        console.log(isNaN(""));        // false: empty string is converted to 0
+        console.log(isNaN(" "));       // false: string with a space is converted to 0
+
+        console.log(isNaN("blabla"));  // true: "blabla" is not a number
+
+        ----------------------------------recomended----------------------------------------------------
+
+        console.log(Number.isNaN(NaN));        // true
+        console.log(Number.isNaN(Number.NaN)); // true
+        console.log(Number.isNaN(0 / 0));      // true
+
+        console.log(Number.isNaN("NaN"));      // false
+        console.log(Number.isNaN(undefined));  // false
+        console.log(Number.isNaN({}));         // false
+        console.log(Number.isNaN("blabla"));   // false
+
+        let a = 0;
+        let b = 0;
+        let result = a / b;
+
+        if (Number.isNaN(result)) {
+        result = "Error: Division resulted in NaN";
+        }
+
+        console.log(result); // "Error: Division resulted in NaN"
 
 
 
-
-
-
-
-
-
-
+        
                */
   

f64f3fb4d589e81d4e177f1e49c2a70a56348858
Chalitha Laksahan <141056409+chalithalaksahan@users.noreply.github.com>
2026-02-03 23:24:16 +0530

    learn What Is the Purpose of Functions

---
+++ JavaScirpt/function.js
@@ -0,0 +1,27 @@
+/*
+               //////////////////////////////////////////////////////////////////////////////////////
+                                  What Is the Purpose of Functions, and How Do They Work?
+               /////////////////////////////////////////////////////////////////////////////////////
+
+* A function call, or invocation, is when we actually use or execute the function. To call a function, you will need to reference 
+the function name followed by a set of parentheses:
+
+function greet() {
+  console.log("Hello, Jessica!");
+}
+
+greet(); // "Hello, Jessica!"
+
+----------------------------------------
+
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+
+greet("Alice"); // Hello, Alice!
+greet("Nick"); // Hello, Nick!
+
+----------------------------------------
+
+
+*/
